0341c2994019cfe7258437702bbd0e7379d42035
Frank Miguel Rondon Rodriguez <fmrondon14@gmail.com>
2024-03-23 11:50:47 -0400

    kkkkkk

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    chracter: []
+})
+</script>
+
+<template>
+    <div>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <h3>nombre</h3>
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import Card from "../components/Card.vue"
+
+let character = ref([])
+
+
+async function loadCharacters(){
+  const response = await fetch('http://fmrruapi.com/api/character?page=1')
+  const data = await response.json()
+  charactervaleu = data.results
+  console.log(data)
+}
+loadCharacters()
+</script>
+
+<template>
+  <div class="flex justify-center mt-10">
+    <h1 class="text-5xl">Personajes del Universo de Frank Miguel </h1>
+  </div>
+
+  <div v-for="personaje in character" ::key="character.id">
+  {{ personaje.name }}
+  </div>
+
+  <!--Card :chracter="character"/> -->
+</template>
